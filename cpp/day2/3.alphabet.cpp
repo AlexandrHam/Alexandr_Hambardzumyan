489365aa24aeeb3139e8437a52ed11a3f1502395
AlexandrHam <alexanhambarcumyan@gmail.com>
2022-03-07 17:08:39 +0400

    Done

--- cpp/day2/3.alphabet.cpp
+++ cpp/day2/3.alphabet.cpp
@@ -7,7 +7,7 @@ int main () {
 	char letter;
 	cout << "Input the letter \n";
 	cin >> letter;
-
+	if ((letter >= 65 && letter <= 90) || (letter >= 97 && letter <= 122)) {
 	switch (letter) {
 	  case ('a'):
 	  case ('e'):
@@ -18,58 +18,19 @@ int main () {
 	  case ('E'):
 	  case ('I'):
 	  case ('O'):
-	  case ('U'):
+	  case ('U'):	  
  	    cout << "'" << letter << "' " << "is vowel \n";
-	    break;
-	  case ('b'):
-	  case ('c'):
-	  case ('d'):
-	  case ('f'):
-	  case ('g'):
-	  case ('h'):
-	  case ('j'):
-	  case ('k'):
-	  case ('l'):
-	  case ('m'):
-	  case ('n'):
-	  case ('p'):
-	  case ('q'):
-	  case ('r'):
-	  case ('s'):
-	  case ('t'):
-	  case ('v'):
-	  case ('w'):
-	  case ('x'):
-	  case ('z'):
-	  case ('B'):
-	  case ('C'):
-	  case ('D'):
-	  case ('F'):
-	  case ('G'):
-	  case ('H'):
-	  case ('J'):
-	  case ('K'):
-	  case ('L'):
-	  case ('M'):
-	  case ('N'):
-	  case ('P'):
-	  case ('Q'):
-	  case ('R'):
-	  case ('S'):
-	  case ('T'):
-	  case ('V'):
-	  case ('W'):
-	  case ('X'):
-	  case ('Z'):
-	    cout << "'" << letter << "' " << "is consonant \n";
 	    break;
 	  case ('y'):
 	  case ('Y'):
 	    cout << "'" << letter << "'" << " can be and vowel and consonant \n";
             break;
 	  default:
-	    cout << "That is not a letter \n";
-
-	}	  
+	     cout << "'" << letter << "' " << "is consonant \n";
+	}
+	}
+	else {
+	  cout << "That is not a letter \n";
+	}	
 	return 0;
 }
